8b435a77cd697943cceb9dbdd15ba55141dc43ad
zashani <z.ashani@agentvi.com>
2015-12-21 11:42:46 +0200

    fix AggregateProbabilityLoss another try 10

--- src/caffe/layers/aggregate_probability_loss_layer.cpp
+++ src/caffe/layers/aggregate_probability_loss_layer.cpp
@@ -103,8 +103,7 @@ void AggregateProbabilityLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*
   }
   }
   
-  if(outer_num_ == 0 || count ==0)
-      LOG(ERROR) << outer_num_ << " " << count;
+ 
   if (normalize_) {
     top[0]->mutable_cpu_data()[0] = loss / count;
   } else {
@@ -193,10 +192,10 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
                 {
                     bottom_diff[i * dim + label_value * inner_num_ + j] = 0;
                     for (int k = 0; k < dim; k++) {
-                      if(infogain_mat[label_value * dim + j] != 0) {
+                      if(infogain_mat[label_value * dim + k] != 0) {
                         bottom_diff[i * dim + label_value * inner_num_ + j] += prob_data[i * dim + k * inner_num_ + j];
-                        //if(k != label_value)
-                        //    bottom_diff[i * dim + k * inner_num_ + j] = 0;
+                        if(k != label_value)
+                            bottom_diff[i * dim + k * inner_num_ + j] = 0;
                       }
                       
                     }                
@@ -216,10 +215,7 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
     }
     // Scale gradient
     const Dtype loss_weight = top[0]->cpu_diff()[0];
-    
-    if(outer_num_ == 0 || count ==0)
-      LOG(ERROR) << outer_num_ << " " << count;
-
+ 
     if (normalize_) {
       caffe_scal(prob_.count(), loss_weight / count, bottom_diff);
     } else {
