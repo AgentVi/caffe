c1e82e98a47ebaa675a692e27a729496b07c42c2
zashani <z.ashani@agentvi.com>
2015-12-21 09:36:28 +0200

    fix AggregateProbabilityLoss another try 3

--- src/caffe/layers/aggregate_probability_loss_layer.cpp
+++ src/caffe/layers/aggregate_probability_loss_layer.cpp
@@ -14,6 +14,23 @@ template <typename Dtype>
 void AggregateProbabilityLossLayer<Dtype>::LayerSetUp(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   LossLayer<Dtype>::LayerSetUp(bottom, top);
+  LayerParameter softmax_param(this->layer_param_);
+  softmax_param.set_type("Softmax");
+  softmax_layer_ = LayerRegistry<Dtype>::CreateLayer(softmax_param);
+  softmax_bottom_vec_.clear();
+  softmax_bottom_vec_.push_back(bottom[0]);
+  softmax_top_vec_.clear();
+  softmax_top_vec_.push_back(&prob_);
+  softmax_layer_->SetUp(softmax_bottom_vec_, softmax_top_vec_);
+
+  has_ignore_label_ =
+    this->layer_param_.loss_param().has_ignore_label();
+  if (has_ignore_label_) {
+    ignore_label_ = this->layer_param_.loss_param().ignore_label();
+  }
+  normalize_ = this->layer_param_.loss_param().normalize();
+  
+  
   if (bottom.size() < 3) {
     CHECK(this->layer_param_.infogain_loss_param().has_source())
         << "Infogain matrix source must be specified.";
@@ -43,24 +60,51 @@ void AggregateProbabilityLossLayer<Dtype>::Reshape(
   CHECK_EQ(infogain->channels(), 1);
   CHECK_EQ(infogain->height(), dim);
   CHECK_EQ(infogain->width(), dim);
+  
+  softmax_layer_->Reshape(softmax_bottom_vec_, softmax_top_vec_);
+  softmax_axis_ =
+      bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis());
+  outer_num_ = bottom[0]->count(0, softmax_axis_);
+  inner_num_ = bottom[0]->count(softmax_axis_ + 1);
+  CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count())
+      << "Number of labels must match number of predictions; "
+      << "e.g., if softmax axis == 1 and prediction shape is (N, C, H, W), "
+      << "label count (number of labels) must be N*H*W, "
+      << "with integer values in {0, 1, ..., C-1}.";
+  if (top.size() >= 2) {
+    // softmax output
+    top[1]->ReshapeLike(*bottom[0]);
+  }
 }
 
 
 template <typename Dtype>
 void AggregateProbabilityLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
-  const Dtype* bottom_data = bottom[0]->cpu_data();
+  
+  // The forward pass computes the softmax prob values.
+  softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
+  const Dtype* prob_data = prob_.cpu_data();      
   const Dtype* bottom_label = bottom[1]->cpu_data();
   int num = bottom[0]->num();
   int dim = bottom[0]->count() / bottom[0]->num();
   Dtype loss = 0;
+  int count = 0;
   for (int i = 0; i < num; ++i) {
     int label = static_cast<int>(bottom_label[i]);
     
-    loss -= log(std::max(bottom_data[i * dim + label], Dtype(FLT_MIN)));
-
+    loss -= log(std::max(prob_data[i * dim + label], Dtype(FLT_MIN)));
+    ++count;
   }
-  top[0]->mutable_cpu_data()[0] = loss / num;
+  
+  if (normalize_) {
+    top[0]->mutable_cpu_data()[0] = loss / count;
+  } else {
+    top[0]->mutable_cpu_data()[0] = loss / outer_num_;
+  }
+  if (top.size() == 2) {
+    top[1]->ShareData(prob_);
+  }
 }
 
 template <typename Dtype>
@@ -76,7 +120,7 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
                << " Layer cannot backpropagate to infogain inputs.";
   }
   if (propagate_down[0]) {
-    const Dtype* bottom_data = bottom[0]->cpu_data();
+    const Dtype* prob_data = prob_.cpu_data();
     const Dtype* bottom_label = bottom[1]->cpu_data();
     const Dtype* infogain_mat = NULL;
     if (bottom.size() < 3) {
@@ -86,7 +130,7 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
     }
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     
-    caffe_copy(bottom[0]->count(), bottom_data, bottom_diff);
+    caffe_copy(bottom[0]->count(), prob_data, bottom_diff);
     
     int num = bottom[0]->num();
     int dim = bottom[0]->count() / bottom[0]->num();
@@ -94,11 +138,11 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
         const int label = static_cast<int>(bottom_label[i]);
 
         int high_label = 0;
-        Dtype high_prob = bottom_data[i * dim];
+        Dtype high_prob = prob_data[i * dim];
         for (int k = 1; k < dim; k++) {
-            if (bottom_data[i * dim + k] > high_prob) {
+            if (prob_data[i * dim + k] > high_prob) {
                 high_label = k;
-                high_prob = bottom_data[i * dim + k];
+                high_prob = prob_data[i * dim + k];
             }
         }
         
@@ -109,7 +153,7 @@ void AggregateProbabilityLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>
                 bottom_diff[i*dim + label] = 0;
                 for (int j = 0; j < dim; ++j) {
                   if(infogain_mat[label * dim + j] != 0) {
-                    bottom_diff[i*dim + label] += bottom_data[i * dim + j];
+                    bottom_diff[i*dim + label] += prob_data[i * dim + j];
                     bottom_diff[i * dim + j] = 0;
                   }
                   
